551c00c4c094c3d427d8e8a6225f8689a2d88235
Raphael Thiebault <raphael@yper.fr>
2020-07-16 11:43:19 +0200

    Return for ios

--- ios/Classes/SwiftAppSettingsPlugin.swift
+++ ios/Classes/SwiftAppSettingsPlugin.swift
@@ -2,14 +2,14 @@ import Flutter
 import UIKit
 
 /// Swift app settings plugin with method channel call handler.
-public class SwiftAppSettingsPlugin: NSObject, FlutterPlugin {
+public class SwiftAppSettingsPlugin: NSObject, FlutterPlugin, FlutterApplicationLifeCycleDelegate {
+  var _result: FlutterResult? = nil
+  
   /// Private method to open device settings window
-  private func openSettings(result: @escaping FlutterResult) {
+  private func openSettings() {
       if let url = URL(string: UIApplication.openSettingsURLString) {
         if #available(iOS 10.0, *) {
-            UIApplication.shared.open(url, options: [:], completionHandler: { (success) in
-              result(nil)
-            })
+            UIApplication.shared.open(url, options: [:])
         } else {
             UIApplication.shared.openURL(url)
         }
@@ -20,11 +20,20 @@ public class SwiftAppSettingsPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "app_settings", binaryMessenger: registrar.messenger())
     let instance = SwiftAppSettingsPlugin()
+    registrar.addApplicationDelegate(instance)
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   /// Public handler method for managing method channel calls.
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-      openSettings(result: result)
+    _result = result
+    openSettings()
+  }
+
+  public func applicationDidBecomeActive(_ application: UIApplication) {
+    guard let result = _result else {
+        return
+    }
+    result(nil)
   }
 }
